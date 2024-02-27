05d3988adf6e7455ba17c63a9c00b9a7c242781c
optimaowo <ivan.larinov02@mail.ru>
2024-02-27 09:53:02 +0900

    first commit

---
+++ app/src/main/java/com/example/multiscreen/Argentina.java
@@ -0,0 +1,16 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Argentina extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_argentina);
+    }
+}
